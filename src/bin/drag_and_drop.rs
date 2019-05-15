f41a9014a99f3b3c141c6d512d19871bfab7eadb
Xiang Fan <sfanxiang@gmail.com>
2019-05-15 17:08:41 +0800

    Port drag_and_drop to gtk4

--- src/bin/drag_and_drop.rs
+++ src/bin/drag_and_drop.rs
@@ -15,32 +15,33 @@ use std::env::args;
 fn build_ui(application: &gtk::Application) {
     // Configure button as drag source for text
     let button = gtk::Button::new_with_label("Drag here");
-    let targets = vec![gtk::TargetEntry::new("STRING", gtk::TargetFlags::SAME_APP, 0),
-                       gtk::TargetEntry::new("text/plain", gtk::TargetFlags::SAME_APP, 0)];
-    button.drag_source_set(gdk::ModifierType::MODIFIER_MASK, &targets, gdk::DragAction::COPY);
-    button.connect_drag_data_get(|_, _, s, _, _| {
+    let targets = gdk::ContentFormats::new(&["STRING", "text/plain"]);
+    button.drag_source_set(gdk::ModifierType::MODIFIER_MASK, Some(&targets), gdk::DragAction::COPY);
+    button.connect_drag_data_get(|_, _, s| {
         let data = "I'm data!";
         s.set_text(data);
     });
+    button.set_property_expand(true);
 
     // Configure label as drag destination to receive text
     let label = gtk::Label::new(Some("Drop here"));
-    label.drag_dest_set(gtk::DestDefaults::ALL, &targets, gdk::DragAction::COPY);
-    label.connect_drag_data_received(|w, _, _, _, s, _, _| {
+    label.drag_dest_set(gtk::DestDefaults::ALL, Some(&targets), gdk::DragAction::COPY);
+    label.connect_drag_data_received(|w, _, s| {
         w.set_text(&s.get_text().expect("Couldn't get text"));
     });
+    label.set_property_expand(true);
 
     // Stack the button and label horizontally
     let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    hbox.pack_start(&button, true, true, 0);
-    hbox.pack_start(&label, true, true, 0);
+    hbox.add(&button);
+    hbox.add(&label);
 
     // Finish populating the window and display everything
     let window = gtk::ApplicationWindow::new(application);
     window.set_title("Simple Drag and Drop Example");
     window.set_default_size(200, 100);
     window.add(&hbox);
-    window.show_all();
+    window.show();
 }
 
 fn main() {
