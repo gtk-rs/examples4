55efc977951c23cff9fe94b443d0c18269243412
Xiang Fan <sfanxiang@gmail.com>
2019-05-15 17:21:47 +0800

    Port drag_and_drop_textview to gtk4

--- src/bin/drag_and_drop_textview.rs
+++ src/bin/drag_and_drop_textview.rs
@@ -12,7 +12,7 @@ use std::env::args;
 use gdk::DragAction;
 use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{DestDefaults, TargetFlags};
+use gtk::DestDefaults;
 use url::Url;
 
 fn build_ui(application: &gtk::Application) {
@@ -29,15 +29,16 @@ fn build_ui(application: &gtk::Application) {
     let scrolled_text_view = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
     scrolled_text_view.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
     scrolled_text_view.add(&text_view);
+    scrolled_text_view.set_property_expand(true);
 
     // Configure the text view to accept URI lists from other applications. This allows
     // dragging files & folders from a file browser program onto the textview.
-    let targets = vec![gtk::TargetEntry::new("text/uri-list", TargetFlags::OTHER_APP, 0)];
-    text_view.drag_dest_set(DestDefaults::HIGHLIGHT, &targets, DragAction::COPY);
+    let targets = gdk::ContentFormats::new(&["text/uri-list"]);
+    text_view.drag_dest_set(DestDefaults::HIGHLIGHT, Some(&targets), DragAction::COPY);
 
     // Process any `drag-data-received` events received by the textview. These events include
     // the URL list we're looking for.
-    text_view.connect_drag_data_received(|w, _, _, _, d, _, _| {
+    text_view.connect_drag_data_received(|w, _, d| {
         // Get the text buffer for the TextView and clear it to make it ready to accept new text.
         let buffer = w.get_buffer().unwrap();
         buffer.set_text("");
@@ -57,12 +58,12 @@ fn build_ui(application: &gtk::Application) {
 
     // Pack widgets vertically.
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    vbox.pack_start(&label, false, false, 0);
-    vbox.pack_start(&scrolled_text_view, true, true, 0);
+    vbox.add(&label);
+    vbox.add(&scrolled_text_view);
 
     // Create a new window
     window.add(&vbox);
-    window.show_all();
+    window.show();
 }
 
 fn main() {
