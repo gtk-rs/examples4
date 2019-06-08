9087449f1d31022791ab383106df34993f6d457c
Xiang Fan <sfanxiang@gmail.com>
2019-06-08 19:07:28 +0800

    Build on travis ci

--- build_travis.sh
+++ build_travis.sh
@@ -3,36 +3,22 @@
 set -x
 set -e
 
-if [ "$GTK" = latest -o "$GTK" = "3.24" ]; then
-	BUNDLE="gtk-3.24.0-1"
+if [ "$GTK" = latest -o "$GTK" = "3.96" ]; then
+	BUNDLE="gtk-3.96.0-1"
 	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-		FEATURES=gtk_3_24,futures,gio/v2_44,subclassing
+		FEATURES=futures,gio/v2_44,subclassing
 	else
-		FEATURES=gtk_3_24,gio/v2_44,subclassing
-	fi
-elif [ "$GTK" = "3.22.30" ]; then
-	BUNDLE="gtk-3.22.30-1"
-	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-		FEATURES=gtk_3_22_30,futures,gio/v2_44,subclassing
-	else
-		FEATURES=gtk_3_22_30,gio/v2_44,subclassing
-	fi
-elif [ "$GTK" = "3.18" ]; then
-	BUNDLE="gtk-3.18.1-2"
-	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-		FEATURES=gtk_3_18,futures,gio/v2_44,subclassing
-	else
-		FEATURES=gtk_3_18,gio/v2_44,subclassing
+		FEATURES=gio/v2_44,subclassing
 	fi
 fi
 
 if [ -n "$BUNDLE" ]; then
 	WD="$PWD"
 	cd "$HOME"
-	curl -LO "https://github.com/EPashkin/gtk-bootstrap/releases/download/$BUNDLE/deps.txz"
+	curl -LO "https://github.com/sfanxiang/gtk-bootstrap/releases/download/$BUNDLE/deps.txz"
 	tar xf deps.txz
 	cd "$WD"
-	export PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig"
+	export PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig:$HOME/local/share/pkgconfig"
 fi
 
 PKG_CONFIG_ALLOW_CROSS=1 cargo check $OTHER_TARGET --features "$FEATURES" --jobs 1 "$@"
